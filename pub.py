6bd53283c482edd7d4810ad9a93de06a46fd374e
shizhan1109 <80838084+shizhan1109@users.noreply.github.com>
2024-04-01 16:56:37 +1000

    intial

--- pub.py
+++ pub.py
@@ -1,24 +1,45 @@
+import time
 import paho.mqtt.client as mqtt
 
-# The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, flags, reason_code, properties):
-    print(f"Connected with result code {reason_code}")
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe("$SYS/zhan")
+def on_publish(client, userdata, mid, reason_code, properties):
+    # reason_code and properties will only be present in MQTTv5. It's always unset in MQTTv3
+    try:
+        userdata.remove(mid)
+    except KeyError:
+        print("on_publish() is called with a mid not present in unacked_publish")
+        print("This is due to an unavoidable race-condition:")
+        print("* publish() return the mid of the message sent.")
+        print("* mid from publish() is added to unacked_publish by the main thread")
+        print("* on_publish() is called by the loop_start thread")
+        print("While unlikely (because on_publish() will be called after a network round-trip),")
+        print(" this is a race-condition that COULD happen")
+        print("")
+        print("The best solution to avoid race-condition is using the msg_info from publish()")
+        print("We could also try using a list of acknowledged mid rather than removing from pending list,")
+        print("but remember that mid could be re-used !")
 
-# The callback for when a PUBLISH message is received from the server.
-def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
-
+unacked_publish = set()
 mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-mqttc.on_connect = on_connect
-mqttc.on_message = on_message
+mqttc.on_publish = on_publish
 
-mqttc.connect("mqtt.eclipseprojects.io", 1883, 60)
+mqttc.user_data_set(unacked_publish)
+mqttc.connect("mqtt.eclipseprojects.io")
+mqttc.loop_start()
 
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
-mqttc.loop_forever()
+# Our application produce some messages
+msg_info = mqttc.publish("paho/test/topic", "my message", qos=1)
+unacked_publish.add(msg_info.mid)
+
+msg_info2 = mqttc.publish("paho/test/topic", "my message2", qos=1)
+unacked_publish.add(msg_info2.mid)
+
+# Wait for all message to be published
+while len(unacked_publish):
+    time.sleep(0.1)
+
+# Due to race-condition described above, the following way to wait for all publish is safer
+msg_info.wait_for_publish()
+msg_info2.wait_for_publish()
+
+mqttc.disconnect()
+mqttc.loop_stop()
